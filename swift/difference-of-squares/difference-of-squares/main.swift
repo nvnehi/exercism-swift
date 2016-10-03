79bce6b09fecb1e092cb4d4d2d6037813f9f099f
nvnehi <nvnehi@icloud.com>
2016-10-03 15:26:54 -0400

    added difference of squares solution

---
+++ swift/difference-of-squares/difference-of-squares/main.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+class Squares {
+    // properties
+    var number: Int
+    var squareOfSums: Int {
+        get {
+            var result = 0
+            for n in 1...number {
+                result += n
+            }
+            return Int(pow(Double(result), 2.0))
+        }
+    }
+    var sumOfSquares: Int  {
+        get {
+            var result = 0
+            for n in 1...number {
+                result += Int(pow(Double(n), 2.0))
+            }
+            return result
+        }
+    }
+    var differenceOfSquares: Int  {
+        get {
+            return squareOfSums - sumOfSquares
+        }
+    }
+    
+    // initializers
+    init(_ n: Int) {
+        number = n
+    }
+}
