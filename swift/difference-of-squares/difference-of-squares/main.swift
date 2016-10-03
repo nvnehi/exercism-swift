6be9f080a843b88236d7e94fea8d139c6a719e9b
nvnehi <nvnehi@icloud.com>
2016-10-03 15:55:40 -0400

    removed get since it's implicit with computed properties when set is not present

--- swift/difference-of-squares/difference-of-squares/main.swift
+++ swift/difference-of-squares/difference-of-squares/main.swift
@@ -4,27 +4,21 @@ struct Squares {
     // properties
     let number: Int
     var squareOfSums: Int {
-        get {
-            var result = 0
-            for n in 1...number {
-                result += n
-            }
-            return Int(pow(Double(result), 2.0))
+        var result = 0
+        for n in 1...number {
+            result += n
         }
+        return Int(pow(Double(result), 2.0))
     }
     var sumOfSquares: Int  {
-        get {
-            var result = 0
-            for n in 1...number {
-                result += Int(pow(Double(n), 2.0))
-            }
-            return result
+        var result = 0
+        for n in 1...number {
+            result += Int(pow(Double(n), 2.0))
         }
+        return result
     }
     var differenceOfSquares: Int  {
-        get {
-            return squareOfSums - sumOfSquares
-        }
+        return squareOfSums - sumOfSquares
     }
     
     // initializers
