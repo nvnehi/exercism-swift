da4993c1ea2a0bee98f502fa932ca8f814bf2962
nvnehi <nvnehi@icloud.com>
2016-10-09 18:52:11 -0400

    added space-age solution

--- README.md
+++
@@ -1,3 +0,0 @@
-swift solutions for exercism
-==
-nvnehi
---
+++ swift/space-age/space-age/main.swift
@@ -0,0 +1,62 @@
+// imports
+import Foundation
+
+// types
+struct SpaceAge {
+    // types
+    enum Planets {
+        case Mercury
+        case Venus
+        case Earth
+        case Mars
+        case Jupiter
+        case Saturn
+        case Uranus
+        case Neptune
+    }
+    
+    // properties
+    let seconds: Double
+    
+    // computed properties
+    var onMercury: Double { return convertToEarthYears(from: .Mercury) }
+    var onVenus: Double { return convertToEarthYears(from: .Venus) }
+    var onEarth: Double { return convertToEarthYears() }
+    var onMars: Double { return convertToEarthYears(from: .Mars) }
+    var onJupiter: Double { return convertToEarthYears(from: .Jupiter) }
+    var onSaturn: Double { return convertToEarthYears(from: .Saturn) }
+    var onUranus: Double { return convertToEarthYears(from: .Uranus) }
+    var onNeptune: Double { return convertToEarthYears(from: .Neptune) }
+    
+    // initializers
+    init(_ seconds: Double) {
+        self.seconds = seconds
+    }
+    
+    // methods
+    func convertToEarthYears(from planet: Planets = .Earth) -> Double {
+        let earthSeconds = 31557600.00
+        let earthYears = seconds / earthSeconds
+        let orbitalPeriod: Double
+        let age: Double
+        
+        // convert age on another planet to earth years by dividing by its orbital period
+        switch planet {
+        case .Mercury: orbitalPeriod = 0.2408467
+        case .Venus: orbitalPeriod = 0.61519726
+        case .Earth: orbitalPeriod = 1.0
+        case .Mars: orbitalPeriod = 1.8808158
+        case .Jupiter: orbitalPeriod = 11.862615
+        case .Saturn: orbitalPeriod = 29.447498
+        case .Uranus: orbitalPeriod = 84.016846
+        case .Neptune: orbitalPeriod = 164.79132
+        }
+        
+        age = earthYears / orbitalPeriod
+        
+        // fix precision to the hundreths place
+        return Double(String(format: "%.2f", age))!
+
+    }
+}
+
