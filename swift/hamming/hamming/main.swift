2f92c3c5bfa29d4e45fb84315dd622e6060bd9b5
nvnehi <nvnehi@icloud.com>
2016-10-13 16:49:58 -0400

    restructured hamming solution

--- swift/hamming/hamming/main.swift
+++ swift/hamming/hamming/main.swift
@@ -2,16 +2,26 @@ import Foundation
 
 struct Hamming {
     static func compute(_ firstDNA: String, against secondDNA: String) -> Int? {
-        guard firstDNA.characters.count == secondDNA.characters.count else { return nil }
-        
+        guard firstDNA.hasSameCount(as: secondDNA) else { return nil }
+
+        return firstDNA.compareDifferences(to: secondDNA)
+    }
+}
+
+private extension String {
+    func compareDifferences(to s: String) -> Int? {
         var difference: Int? = 0
-        
-        for i in 0..<firstDNA.characters.count {
-            let idx = firstDNA.index(firstDNA.startIndex, offsetBy: i)
-            
-            if firstDNA[idx] != secondDNA[idx] { difference! += 1 }
+
+        for i in 0..<self.characters.count {
+            let idx = self.index(self.startIndex, offsetBy: i)
+
+            if self[idx] != s[idx] { difference! += 1 }
         }
- 
+
         return difference
+    }
+
+    func hasSameCount(as s: String) -> Bool {
+        return self.characters.count == s.characters.count
     }
 }
