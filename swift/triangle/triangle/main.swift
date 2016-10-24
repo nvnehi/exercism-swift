d52f273318560ccf3d5a01f52161f39b43d87912
nvnehi <nvnehi@icloud.com>
2016-10-24 18:48:31 -0400

    added triangle solution

---
+++ swift/triangle/triangle/main.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+struct Triangle {
+    private enum Kind: String {
+        case Equilateral, Isosceles, Scalene, ErrorKind
+    }
+    
+    var kind = Kind.ErrorKind.rawValue
+    
+    init(_ x: Double, _ y: Double, _ z: Double) {
+        guard (x > 0 && y > 0 && z > 0) && (x <= y + z && z <= x + y) else { return }
+        
+        if x == y && y == z {
+            kind = Kind.Equilateral.rawValue
+        } else if x == y || x == z || y == z {
+            kind = Kind.Isosceles.rawValue
+        } else {
+            kind = Kind.Scalene.rawValue
+        }
+    }
+}
